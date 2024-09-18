f916dde7905758b86be023352278f37cca0b860a
Abdullah Althobetey <abdullah.th@me.com>
2024-09-18 20:08:21 +0300

    add validations to template

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,2 +1,12 @@
 class Template < ApplicationRecord
+  validates :name, presence: true
+  validate :must_contain_placeholder
+
+  private
+
+  def must_contain_placeholder
+    unless body.include?("{{placeholder}}")
+      errors.add :body, "must contain {{placeholder}} somewhere in the template"
+    end
+  end
 end
